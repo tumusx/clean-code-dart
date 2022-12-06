926687b02d7f49b0ae388716d71b579449befd9b
murillo <tumusx1@gmail.com>
2022-12-06 12:52:59 -0300

    feat: adicionado repository para professor e aluno

--- bin/solid/SRP/professor.dart
+++ bin/solid/SRP/professor.dart
@@ -1,1 +1,36 @@
+import 'dart:ffi';
 
+class Professor {
+  late String nameProfessor;
+  late List<String> materiasProfessor;
+  late int idadeProfessor;
+  late List<String> turmarProfessor;
+
+  Professor(this.nameProfessor, this.materiasProfessor, this.idadeProfessor, this.turmarProfessor);
+}
+
+class Alunos {
+  late String nameAluno;
+  late List<String> materiasDoAluno;
+  late int idadeAluno;
+  late String turmaAluno;
+
+  Alunos(this.idadeAluno, this.nameAluno, this.materiasDoAluno, this.turmaAluno);
+}
+
+abstract class IAlunoRepository {
+  Future<Bool> cadastrarAluno(Alunos aluno);
+}
+
+abstract class IProfessorRepository {
+  Future<Bool> cadastrarProfessor(Professor professor);
+}
+
+
+class ProfessorRepositoryImpl implements IProfessorRepository{
+  @override
+  Future<Bool> cadastrarProfessor(Professor professor) async {
+    await professor.idadeProfessor = 10
+    return true;
+  }
+}
